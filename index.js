a30b4867a8a5b3ceeea7cee569ac6227c816e0ee
Joseph Parker <jparker010190@gmail.com>
2018-07-14 03:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function lowerCaseDrivers(drivers){
   return d
 }
 function nameToAttributes(drivers){
-return const d = drivers.map(function(num){
+return drivers.map(function(num){
     return  {firstName: num.split(" ")[0], lastName: num.split(" ")[1]}
 
   })
